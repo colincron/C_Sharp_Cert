ca058cdf14a50a07d90b6cdfc1c9ddc4bcdcb01a
Colin <colint.cron@gmail.com>
2024-04-25 13:59:56 -0400

    Store and iterate through sequences of data using Arrays and the foreach statement in C#

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿// https://learn.microsoft.com/en-us/training/modules/csharp-arrays/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+        System.Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+        System.Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+        System.Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+        fraudulentOrderIDs[0] = "F000";
+
+        System.Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+        System.Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+    }
+}
